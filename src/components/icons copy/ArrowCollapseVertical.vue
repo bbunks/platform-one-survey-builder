104093ea4f3a0edaa3acee8e27eb4af057edc366
Brenden Bunker <brendenbunker@gmail.com>
2023-05-08 13:12:42 -0600

    removed icon backups

--- src/components/icons copy/ArrowCollapseVertical.vue
+++
@@ -1,4 +0,0 @@
-<template>
-<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M4.25 13H20.25V15H4.25V13ZM4.25 10H20.25V12H4.25V10ZM16.25 5L12.25 9L8.25 5H11.25V2H13.25V5H16.25ZM8.25 20L12.25 16L16.25 20H13.25V23H11.25V20H8.25Z" fill="white"/>
-</svg>
